527cc62ccc05adb9229027b7bd07f04c410cf65f
AkasameVN26 <thaibinhakashi2@gmail.com>
2025-07-08 23:38:05 +0700

    fix dark mode #1

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "about-binh",
+  base: "/about-binh",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
